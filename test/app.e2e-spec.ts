bd29f786c83108de7f861894324190e682a69d1c
Camilo Serrano <camilo.serrano@bybeeconcept.com>
2025-02-13 07:42:34 -0500

    E2E test for auth implemented

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,8 +2,9 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import mongoose from 'mongoose';
 
-describe('AppController (e2e)', () => {
+describe('Book & Auth Controller (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
@@ -15,10 +16,40 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
-  });
+  beforeAll(() => {
+    mongoose.set("strictQuery", false);
+    mongoose.connect(process.env.DB_URI, function () {
+      mongoose.connection.db.dropDatabase()
+    })
+  })
+
+  afterAll(() => mongoose.disconnect())
+
+  const user = {
+    name: 'Ghulam',
+    email: 'ghulam@gmail.com',
+    password: '12345678',
+  };
+
+  describe('Auth', () => {
+    it('(POST) - Register a new user', () => {
+      return request(app.getHttpServer())
+        .post('/auth/signup')
+        .send(user)
+        .expect(201)
+        .then((res) => {
+          expect(res.body.token).toBeDefined();
+        });
+    });
+  
+    it('(GET) - Login user', () => {
+      return request(app.getHttpServer())
+        .get('/auth/login')
+        .send({ email: user.email, password: user.password })
+        .expect(200)
+        .then((res) => {
+          expect(res.body.token).toBeDefined();
+        });
+    });
+  })
 });
